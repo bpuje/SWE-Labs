c60501f3ddf8de1165a6271125ef6f6d54bfef49
Purevdemberel <purevdemberel@gmail.com>
2019-10-12 00:43:32 -0500

    lab8assign

--- lab8eRegister/src/main/java/edu/mum/cs/cs425/eregister/lab8/repository/StudentRepository.java
+++
@@ -1,4 +0,0 @@
-package edu.mum.cs.cs425.eregister.lab8.repository;
-
-public class StudentRepository {
-}
--- lab8eRegister/src/main/java/edu/mum/cs/cs425/eregister/lab8/service/impl/StudentServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package edu.mum.cs.cs425.eregister.lab8.service.impl;
-
-public class StudentServiceImpl {
-}
